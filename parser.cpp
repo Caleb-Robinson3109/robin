10a65b40a58344016f40a9a3c425876c4d1ff290
Caleb <calebrobinson3109@gmail.com>
2025-08-02 16:07:36 -0500

    added parse Main and maindef

--- parser.cpp
+++ parser.cpp
@@ -14,7 +14,7 @@ AST parser (vector<Token>& tokens){
         return parsed.ast;
     }
     else{
-        Node error("error", "error", false);
+        Node error("error", Token(), false);
         return AST(error);
     }
 }
@@ -25,13 +25,57 @@ Ret parse_Program(vector<Token>& tokens, int index){
         return parsed_Main;
     }
     else{
-        Ret error(false, AST(Node("error", "error", false)), index);
+        Ret error(false, Node("error", Token(), false), index);
         return error;
     }
 }
 
-Ret parse_Main(vector<Token>& tokens, int index);
-Ret parse_MainDef(vector<Token>& tokens, int index);
+Ret parse_Main(vector<Token>& tokens, int index){
+    Ret parsed_MainDef = parse_MainDef(tokens, index);
+    AST Main(Node("Main", Token(), false));
+    if(parsed_MainDef.valid){
+        Ret parsed_FuncBody = parse_FuncBody(tokens, parsed_MainDef.index);
+        Main.getRoot().addChild(parsed_MainDef.ast);
+        if(parsed_FuncBody.valid){
+            Main.getRoot().addChild(parsed_FuncBody.ast);
+            return Ret(true, Main.getRoot(), parsed_FuncBody.index);
+        }
+        else{
+            Ret error(false, Node("error", Token(), false), index);
+            return error;
+        }
+    }
+    else{
+        Ret error(false, Node("error", Token(), false), index);
+        return error;
+    }
+}
+
+Ret parse_MainDef(vector<Token>& tokens, int index){
+    AST MainDef(Node("MainDef", Token(), false));
+    if(tokens.at(index).type == "kw_main"){
+        MainDef.getRoot().addChild(Node("kw_main", tokens.at(index), true));
+        index++;
+        if(tokens.at(index).type == "kw_arrow"){
+            MainDef.getRoot().addChild(Node("kw_arrow", tokens.at(index), true));
+            index++;
+            if(tokens.at(index).type == "kw_int"){
+                MainDef.getRoot().addChild(Node("kw_int", tokens.at(index), true));
+                index++;
+                Ret okay(true, MainDef.getRoot(), index);
+            }
+        }
+        else{
+            Ret error(false, Node("error", Token(), false), index);
+            return error;
+        }
+    }
+    else{
+        Ret error(false, Node("error", Token(), false), index);
+        return error;
+    }
+}
+
 Ret parse_FuncBody(vector<Token>& tokens, int index);
 Ret parse_Scope(vector<Token>& tokens, int index);
 Ret parse_ScopeType(vector<Token>& tokens, int index);
