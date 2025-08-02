92d7128f511929926136b04da976ecab9f9f43c9
Caleb <calebrobinson3109@gmail.com>
2025-08-02 10:18:10 -0500

    some work to parser

--- parser.cpp
+++ parser.cpp
@@ -1,26 +1,35 @@
-#ifndef PARSER_HPP
-#define PARSER_HPP
-
 #include <vector>
 
 #include "token.hpp"
 #include "ast.hpp"
+#include "parser.hpp"
 
 using namespace std;
 
-class Ret {
-public:
-    bool valid;
-    AST ast;
-    int index;
+AST parser (vector<Token>& tokens){
+    int index = 0;
+    Ret parsed = parse_Program(tokens, index);
 
-    Ret(bool v, const AST& a, int i)
-        : valid(v), ast(a), index(i) {}
-};
+    if(parsed.valid){
+        return parsed.ast;
+    }
+    else{
+        Node error("error", "error", false);
+        return AST(error);
+    }
+}
 
+Ret parse_Program(vector<Token>& tokens, int index){
+    Ret parsed_Main = parse_Main(tokens, index);
+    if(parsed_Main.valid){
+        return parsed_Main;
+    }
+    else{
+        Ret error(false, AST(Node("error", "error", false)), index);
+        return error;
+    }
+}
 
-AST parser (vector<Token>& tokens);
-Ret parse_Program(vector<Token>& tokens, int index);
 Ret parse_Main(vector<Token>& tokens, int index);
 Ret parse_MainDef(vector<Token>& tokens, int index);
 Ret parse_FuncBody(vector<Token>& tokens, int index);
@@ -33,5 +42,3 @@ Ret parse_Statement(vector<Token>& tokens, int index);
 Ret parse_IO(vector<Token>& tokens, int index);
 Ret parse_Return(vector<Token>& tokens, int index);
 Ret parse_Output(vector<Token>& tokens, int index);
-
-#endif
